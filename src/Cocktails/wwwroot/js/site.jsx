962ffd7e411eb96aeaa66804230ea9f61aa317db
Lauri Harpf <lauri.harpf@acelvia.com>
2017-01-08 18:34:13 +0200

    Minor refactoring

--- src/Cocktails/wwwroot/js/site.jsx
+++ src/Cocktails/wwwroot/js/site.jsx
@@ -1,15 +1,15 @@
 ﻿var CocktailButton = React.createClass({
-    render: function() {
+    render: function () {
         var src = "/images/cocktail_no_image_extra_small.jpg";
-        if (this.props.image) {
-            src = "/images/drinks/" + this.props.image;
+        if (this.props.cocktail.image) {
+            src = "/images/drinks/" + this.props.cocktail.image;
         }
 
         return (
-            <div className="col-sm-4 cocktailButton" style={{ textAlign: "center" }} data-toggle="modal" data-target={"#modal" + this.props.id}>                
+            <div className="col-sm-4 cocktailButton" style={{ textAlign: "center" }} data-toggle="modal" data-target={"#modal" + this.props.cocktail.id}>
                 <img src={src} width="50" height="78" />
-                <h6>{this.props.name}</h6>
-                <CocktailDetails id={this.props.id} name={this.props.name} ingredients={this.props.ingredients} instructions={this.props.instructions} />
+                <h6>{this.props.cocktail.name}</h6>
+                <CocktailDetails cocktail={this.props.cocktail} />
             </div>
         );
     }
@@ -17,17 +17,17 @@
 
 var CocktailDetails = React.createClass({
     render: function() {
-        var ingredients = this.props.ingredients.map(function(ingredient) {
+        var ingredients = this.props.cocktail.ingredients.map(function(ingredient) {
             return (
                 <li key={ingredient}>{ingredient}</li>
             );
         });
         return (
-            <div className="modal fade" id={"modal" + this.props.id} tabIndex="-1" role="dialog" aria-labelledby={"modalLabel" + this.props.id} aria-hidden="true">
+            <div className="modal fade" id={"modal" + this.props.cocktail.id} tabIndex="-1" role="dialog" aria-labelledby={"modalLabel" + this.props.cocktail.id} aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h4 className="modal-title" id={"modalLabel" + this.props.id}>{this.props.name}</h4>
+                            <h4 className="modal-title" id={"modalLabel" + this.props.cocktail.id}>{this.props.cocktail.name}</h4>
                         </div>
                         <div className="modal-body">
                             <h6>Ingredients</h6>
@@ -36,7 +36,7 @@ var CocktailDetails = React.createClass({
                             </ul>
 
                             <h6>Instructions</h6>
-                            {this.props.instructions}
+                            {this.props.cocktail.instructions}
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-primary">Close</button>
@@ -65,7 +65,7 @@ var CocktailGrid = React.createClass({
     render: function () {
         var cocktails = this.state.data.map(function(cocktail) {
             return (
-                <CocktailButton key={cocktail.id} id={cocktail.id} name={cocktail.name} ingredients={cocktail.ingredients} instructions={cocktail.instructions} image={cocktail.image} />
+                <CocktailButton key={cocktail.id} cocktail={cocktail} />
             );
         });
         return (

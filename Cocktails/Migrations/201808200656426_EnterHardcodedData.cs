8a7de543e00e239d2867fd8c0e7ac699d6b341c5
Lauri Harpf <lauri.harpf@acelvia.com>
2018-08-20 15:42:36 +0300

    The EF migration that ran happily on my local machine didn't run in Azure. After some searching, https://stackoverflow.com/a/48539230/843984 seems like a potential solution, so let's give that a go.

--- Cocktails/Migrations/201808200656426_EnterHardcodedData.cs
+++ Cocktails/Migrations/201808200656426_EnterHardcodedData.cs
@@ -6,15 +6,16 @@ namespace Cocktails.Migrations
     public partial class EnterHardcodedData : DbMigration
     {
         public override void Up()
-        {
+        {            
             // Delete all existing data
             Sql("DELETE FROM dbo.RecipeRow");
             Sql("DELETE FROM dbo.Cocktail");
             Sql("DELETE FROM dbo.Ingredient");
             // Add new, hardcoded data
-            SqlFile("../Migrations/201808200656426_dbo.Cocktail.data.sql");
-            SqlFile("../Migrations/201808200656426_dbo.Ingredient.data.sql");
-            SqlFile("../Migrations/201808200656426_dbo.RecipeRow.data.sql");
+            String resourcePrefix = typeof(EnterHardcodedData).Namespace;
+            SqlResource(resourcePrefix + ".201808200656426_dbo.Cocktail.data.sql");
+            SqlResource(resourcePrefix + ".201808200656426_dbo.Ingredient.data.sql");
+            SqlResource(resourcePrefix + ".201808200656426_dbo.RecipeRow.data.sql");
         }
         
         public override void Down()

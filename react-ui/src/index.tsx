eaac05a2e1541b2d679f30541b17381c75e64ae2
Lauri Harpf <lauri.harpf@acelvia.com>
2021-05-29 11:29:21 +0300

    Add typescript to the react app

---
+++ react-ui/src/index.tsx
@@ -0,0 +1,40 @@
+import "./jquery";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.min";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import { ConnectedRouter } from "connected-react-router";
+import { configureStore } from "./Redux/store";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+declare global {
+  interface Window {
+    initialReduxState: any;
+  }
+}
+
+// Create browser history to use in the Redux store
+const baseUrl =
+  document.getElementsByTagName("base")[0].getAttribute("href") || "";
+const history = createBrowserHistory({ basename: baseUrl });
+
+// Get the application-wide store instance, prepopulating with state from the server where available.
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  rootElement
+);
+
+registerServiceWorker();
